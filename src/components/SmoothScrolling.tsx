a5cc44aa8b16f2fc22447f2d3c3e8d2c72b4c8cb
talha <talhairfan1947@gmail.comm>
2026-04-26 19:00:52 +0500

    ok

--- src/components/SmoothScrolling.tsx
+++ src/components/SmoothScrolling.tsx
@@ -1,11 +1,27 @@
 "use client";
 
 import { ReactLenis } from 'lenis/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function SmoothScrolling({ children }: { children: React.ReactNode }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
+  if (isMobile) {
+    return <>{children}</>;
+  }
+
   return (
-    <ReactLenis root options={{ lerp: 0.05, duration: 2, smoothWheel: true, syncTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.05, duration: 2, smoothWheel: true }}>
       {children}
     </ReactLenis>
   );
